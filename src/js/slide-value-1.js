8cd16ff50e3c3f6df93ac717077ce493f461ae6a
Dao Trung Duc <daotrungduc.it@gmail.com>
2025-10-03 11:56:45 +0700

    feat: value-1

--- src/js/slide-value-1.js
+++ src/js/slide-value-1.js
@@ -1,16 +1,30 @@
-// const intervalValue = setInterval(() => {
-//   const slider = document.querySelector(".value-slider");
-//   if (slider) {
-//     clearInterval(intervalValue);
-//     const btnLeft = document.querySelector(".btn-value-left");
-//     const btnRight = document.querySelector(".btn-value-right");
+const intervalValue = setInterval(() => {
+  const slider = document.querySelector(".value1-slider");
 
-//     btnLeft.addEventListener("click", () => {
-//       slider.scrollLeft -= slider.children[1].offsetWidth + 16;
-//     });
+  if (slider) {
+    clearInterval(intervalValue);
+    let dem = 0;
 
-//     btnRight.addEventListener("click", () => {
-//       slider.scrollLeft += slider.children[1].offsetWidth + 16;
-//     });
-//   }
-// }, 100);
+    const value1Item = document.querySelectorAll(".value1-item");
+    let tong = value1Item.length;
+    const btnLeft = document.querySelector(".btn-value-left");
+    const btnRight = document.querySelector(".btn-value-right");
+
+    btnLeft.addEventListener("click", () => {
+      value1Item[dem].classList.toggle("hidden");
+      if (dem == 0) {
+        dem = tong;
+      }
+      dem = (dem - 1) % tong;
+      value1Item[dem].classList.toggle("hidden");
+      console.log(dem);
+    });
+
+    btnRight.addEventListener("click", () => {
+      value1Item[dem].classList.toggle("hidden");
+      dem = (dem + 1) % tong;
+      value1Item[dem].classList.toggle("hidden");
+      console.log(dem);
+    });
+  }
+}, 100);
