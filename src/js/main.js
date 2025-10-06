6470cb40a9b951b9aa86de7735b2069ad0ec2865
Dao Trung Duc <daotrungduc.it@gmail.com>
2025-10-06 11:56:52 +0700

    feat: plans

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,7 @@ loadComponents("value-1", "value-1.html");
 loadComponents("value-2", "value-2.html");
 loadComponents("feature", "feature.html");
 loadComponents("functions", "functions.html");
-// loadComponents("plans", "plans.html");
+loadComponents("plans", "plans.html");
 // loadComponents("tutorial", "tutorial.html");
 // loadComponents("contact", "contact.html");
 // loadComponents("footer", "footer.html");
